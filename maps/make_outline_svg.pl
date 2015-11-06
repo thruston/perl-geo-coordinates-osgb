4f3522876618569caf94dce1f3d7fb324ce147c5
Toby Thurston <thurston@eml.cc>
2015-11-06 19:13:36 +0000

    Update outline maker to draw insets with connecting lines

--- maps/make_outline_svg.pl
+++ maps/make_outline_svg.pl
@@ -2,33 +2,51 @@ use strict;
 use warnings;
 use Geo::Coordinates::British_Maps qw(%maps);
 
-sub plot_poly {
-    my ($path, $s) = @_;
-    my $out = "p:=$path; draw p withcolor orange;";
-    my $color  = $s =~ m{\AOL}osxi ? 'red' : 'blue';
-    my $adjust = $s =~ m{\AOL}osxi ? '+2' : '-2'; 
-    if ($s !~ m{Inset}xios ) {
-        $out .= "label(\"$s\" infont \"phvb8r\" scaled 0.5, center p shifted (0,$adjust)) withcolor .6[$color,white];"; 
-    }
-    $out .= "\n";
-    return $out;
+sub overlap_or_touch {
+    my ($a,$b) = @_;
+    return !($b->[0][0] > $a->[1][0]
+          || $b->[1][0] < $a->[0][0]
+          || $b->[1][1] < $a->[0][1]
+          || $b->[0][1] > $a->[1][1]);
 }
-
 my $series_wanted = (@ARGV > 0) ? $ARGV[0] : 'A';
 
 my $mp_file = 'outlines.mp';
 
 open(my $plotter, '>', $mp_file);
 print $plotter "prologues := 3; outputtemplate := \"outlines$series_wanted.svg\"; outputformat:=\"svg\";\n";  
-print $plotter 'beginfig(1); path p; color orange; orange = ( 221/255, 61/255, 31/255 );', "\n";
+print $plotter "beginfig(1); path p[]; color orange, pink, red;\n"; 
+print $plotter "orange = ( 221/255, 61/255, 31/255 ); pink = (224/255,36/255,114/255); red = (228/255, 0, 28/255);\n";
 
 my @draws;
-while (my ($k, $m) = each %maps) {
+my @inset_connectors;
+my $i = 0;
+my %path_number_for = ();
+for my $k (sort keys %maps) {
     my ($series, $label) = split ':', $k;
     my $p = index($series_wanted, $series);
     next if $p < 0;
+    $path_number_for{$k} = $i++;
+    my $m = $maps{$k};
     my $path = join '--', map { sprintf "(%.2f,%.2f)", $_->[0]/1000, $_->[1]/1000 } @{$m->{polygon}}; 
-    push @draws, plot_poly($path, $label); 
+    my $color = $series eq 'A' ? 'pink' : $series eq 'B' ? 'orange' : $series eq 'C' ? 'red' : 'black';
+    my $out = sprintf 'p%d:=%s; draw p%d withcolor %s;', $path_number_for{$k}, $path, $path_number_for{$k}, $color; 
+    # adjustments for OL maps
+    my $textcolor = $label =~ m{\AOL}osxi ? 'blue' : $color;
+    my $adjust    = $label =~ m{\AOL}osxi ? '+2' : '-2'; 
+    if ( $k eq $m->{parent} ) {
+        $out .= "label(\"$label\" infont \"phvb8r\" scaled 0.5, center p$path_number_for{$k} shifted (0,$adjust)) withcolor .5[$textcolor,white];"; 
+    }
+    elsif ( overlap_or_touch($m->{bbox}, $maps{$m->{parent}}->{bbox} )) {
+        #warn "No connector for $k\n";
+    }
+    else {
+        push @inset_connectors, sprintf "draw center p%d -- center p%d cutbefore p%d cutafter p%d dashed withdots scaled 0.2  withcolor %s;\n",
+                                $path_number_for{$k}, $path_number_for{$m->{parent}},
+                                $path_number_for{$k}, $path_number_for{$m->{parent}},
+                                $color;
+    }
+    push @draws, "$out\n";
 }
 
 print $plotter "drawoptions(withpen pencircle scaled 0.2 withcolor .7 white);\n";
@@ -37,6 +55,7 @@ print $plotter 'for i=1 upto  6: draw (100i,0) -- (100i,1300); endfor',"\n";
 
 print $plotter "drawoptions(withpen pencircle scaled 0.2);\n";
 print $plotter @draws;
+print $plotter @inset_connectors;
 
 print $plotter "endfig;end.\n";
 close $plotter;
